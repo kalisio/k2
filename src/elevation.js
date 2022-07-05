5c85250aa1ba0d13785bd5be079c638bf511e3ee
Robin Bourianes <robin.bourianes@kalisio.com>
2022-07-05 15:01:30 +0200

    wip: better point sampling over the segments.

--- src/elevation.js
+++ src/elevation.js
@@ -88,6 +88,7 @@ async function elevation(geojson) {
   const concurrency = _.get(geojson, 'concurrency', 4)
   const demOverride = _.get(geojson, 'demOverride', '')
   const corridorWidth = _.get(geojson, 'corridorWidth', 0)
+  const halfCorridorWidth = Math.max(1, corridorWidth / 2)
 
   // 1 arc sec is ~30m at the equator (~ 0.0002778deg)
   // srtmv4 is 3arcsec => ~90m
@@ -102,47 +103,84 @@ async function elevation(geojson) {
   }
   demFile = path.join('/mbtiles', demFile)
 
-  const halfCorridorWidth = Math.max(1, corridorWidth / 2)
-
-  debug(`using ${demFile} concurrency ${concurrency}`)
-
    // prepare work folder
   const workDir = fs.mkdtempSync(path.join(os.tmpdir(), 'elevation-'))
-  debug(`working directory: ${workDir}`)
+
+  debug(`using ${demFile} concurrency ${concurrency} corridor width ${corridorWidth}`)
+  debug(`working directory ${workDir}`)
 
   let feature
   if (geojson.type === 'FeatureCollection') feature = geojson.features[0]
   else feature = geojson
 
-  // foreach segment, generate elevation profile as tiff
-  // compute segment's length, generate as many point to match
-  // requested resolution
-  const allTasks = []
+  let totalDistance = 0
+  let skipFirstPoint = false
+  const allSegments = []
   turf_segmentEach(feature, (segment) => {
-    const [ lon0, lat0 ] = segment.geometry.coordinates[0]
-    const [ lon1, lat1 ] = segment.geometry.coordinates[1]
+    const [ lon0, lat0 ] = segment.geometry.coordinates[0]
+    const [ lon1, lat1 ] = segment.geometry.coordinates[1]
     const length = turf_length(segment, { units: 'kilometers' }) * 1000
-    const numPoints = Math.floor(length / resolution)
-    debug(`segment ${allTasks.length} ${numPoints} points with res ${resolution} (len=${length} m).`)
 
-    const outFile = path.join(workDir, `segment_${allTasks.length}.tif`)
-    const logFile = path.join(workDir, `segment_${allTasks.length}.log`)
-    const projStr = `+proj=tpeqd +lon_1=${lon0} +lat_1=${lat0} +lon_2=${lon1} +lat_2=${lat1}`
+    // how many points will we sample on this segment ?
+    const t0 = (totalDistance / resolution) + (skipFirstPoint ? 1 : 0)
+    const t1 = (totalDistance + length) / resolution
+    const numPoints = Math.floor(t1) - Math.ceil(t0)
+
+    debug(`segment ${allSegments.length} numPoints ${numPoints} [${t0}, ${t1}] start offset ${t0 - Math.trunc(t0)} `)
+
+    // we'll have to skip first point on next segment
+    // if the endpoint of this segment must be sampled
+    skipFirstPoint = t1 === Math.floor(t1)
+
+    // compute gdalwarp extent parameters
+    // minx and maxx are in meters relative to 0 where 0 is the center of the segment
+    let maxx = length / 2
+    let minx = -maxx
+    // adjust minx to fall on first sampled point of segment (might not be the first point)
+    minx += resolution * (t0 - Math.trunc(t0))
+    // adjust maxx to fall on last sampled point of segment (might not be the endpoint)
+    maxx -= resolution * (t1 - Math.trunc(t1))
+    // now adjust both by half the pixel size since we sample at pixel center
+    minx -= resolution / 2
+    maxx += resolution / 2
+
+    debug(`segment ${allSegments.length} minx maxx [${-length/2}, ${length/2}] adjusted to [${minx}, ${maxx}]`)
+
+    allSegments.push({
+      segment,
+      numPoints,
+      projStr: `+proj=tpeqd +lon_1=${lon0} +lat_1=${lat0} +lon_2=${lon1} +lat_2=${lat1}`,
+      minx,
+      maxx,
+      offset: t0 - Math.trunc(t0)
+    })
+
+    totalDistance += length
+  })
+
+  if (!skipFirstPoint) {
+    // extend last segment's maxx to sample past the endpoint
+    allSegments[allSegments.length - 1].numPoints += 1
+    allSegments[allSegments.length - 1].maxx += resolution
+  }
+
+  const allTasks = []
+  allSegments.forEach((segment) => {
+    if (segment.numPoints === 0) return
+
+    const outFile = path.join(workDir, `task_${allTasks.length}.tif`)
+    const logFile = path.join(workDir, `task_${allTasks.length}.log`)
 
     const task = {
       tif: outFile,
-      segment,
+      segment: segment,
       job: async () => {
-        const cs2csOut = shell_scrap(`printf "${lat0} ${lon0}\\n${lat1} ${lon1}" | cs2cs EPSG:4326 +to ${projStr}`).split('\n').filter(e => e !== '')
-        const minx = cs2csOut[0].split('\t')[0]
-        const maxx = cs2csOut[1].split('\t')[0]
-
         return exec_bg('gdalwarp', [
-                      '-te', minx, `-${halfCorridorWidth}`, maxx, halfCorridorWidth,
-                      '-t_srs', `"${projStr}"`,
-                      '-ts', numPoints, '1',
-                      '-r', 'max',
-                      demFile, outFile ], logFile)
+          '-t_srs', `"${segment.projStr}"`,
+          '-te', segment.minx, -halfCorridorWidth, segment.maxx, halfCorridorWidth,
+          '-ts', segment.numPoints, '1',
+          '-r', 'max',
+          demFile, outFile ], logFile)
       },
       success: () => {},
       fail: () => {}
@@ -158,10 +196,11 @@ async function elevation(geojson) {
     const tif = await GeoTIFF.fromFile(task.tif)
     const img = await tif.getImage()
     const res = img.getResolution()
+    debug(`${task.tif} res [${res[0]}, ${res[1]}]`)
     const data = await img.readRasters()
     // fill geojson point list
     segments.push(Array.from(data[0], (v, i) => {
-      const point = turf_along(task.segment, i * res[0], { units: 'meters' })
+      const point = turf_along(task.segment.segment, (task.segment.offset + i) * res[0], { units: 'meters' })
       point.properties.z = v
       return point
     }))
