e0068390e7c15e615f94b28e609d2b4ca86fc794
Luc Claustres <luc.claustres@orange.fr>
2025-09-19 18:49:51 +0200

    fix: removed code climate reporting to make tests run

--- scripts/init_runner.sh
+++ scripts/init_runner.sh
@@ -12,7 +12,7 @@ THIS_DIR=$(dirname "$THIS_FILE")
 ### Github Actions
 
 init_github_run_tests() {
-    install_reqs age sops nvm node20 cc_test_reporter
+    install_reqs age sops nvm node20
 }
 
 init_github_additional_tests() {
