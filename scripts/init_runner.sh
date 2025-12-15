977d21c911a60b406e43445f0285ab40cc7a6d5e
Sacha <sacha.colbert-lisbona@kalisio.com>
2025-12-15 16:41:48 +0100

    feat: implement sonarqube

--- scripts/init_runner.sh
+++ scripts/init_runner.sh
@@ -12,7 +12,7 @@ THIS_DIR=$(dirname "$THIS_FILE")
 ### Github Actions
 
 init_github_run_tests() {
-    install_reqs age sops nvm node20
+    install_reqs age sops nvm node20 sonar_scanner_cli
 }
 
 init_github_additional_tests() {
